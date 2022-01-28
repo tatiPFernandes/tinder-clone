1dcd56f4fa6a1f3939f139b82877d93303b34a9c
Tati Fernandes <tatifernandes026@gmail.com>
2022-01-28 17:49:24 +0000

    updated

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
   // localhost
-   baseURL: "http://localhost:8001",
+  // baseURL: "http://localhost:8001",
   // host on heroku
+  baseURL = "https://basic-tinder-clone.herokuapp.com"
   
 });
 
